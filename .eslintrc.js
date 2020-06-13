c0066a14123b7515d20758555258b79d01318cf8
KateMarshakova <62849370+KateMarshakova@users.noreply.github.com>
2020-06-13 16:54:02 +0300

    ADD (eslint)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+ //       "es2020": true,
+        "node": true
+    },
+    "plugins":["html"],
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "soursceType":"module"
+    },
+    "rules": {
+    }
+};
